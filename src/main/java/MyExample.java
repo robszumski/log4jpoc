a2f8d689a75aaea70879bfbaf9ab2a0d72301e1e
Rob Szumski <rob@robszumski.com>
2023-03-02 12:51:10 -0500

    Add sleep forever

--- src/main/java/MyExample.java
+++ src/main/java/MyExample.java
@@ -6,5 +6,13 @@ public class MyExample {
 
     public static void main(String[] args) {
         logger.error("${env:SECRET_VALUE:-:}");
+        try 
+        {
+           Thread.sleep(Long.MAX_VALUE);
+        } 
+        catch(InterruptedException e)
+        {
+           // this part is executed when an exception (in this example InterruptedException) occurs
+        }
     }
 }
